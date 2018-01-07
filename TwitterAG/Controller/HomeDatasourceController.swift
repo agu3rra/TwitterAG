9ce463cb5b3b77f3866f0fca211b95118b4a65a0
Andre Guerra <agu3rra@gmail.com>
2018-01-07 16:46:35 -0200

    Added dynamic cell size. Ep 6.

--- TwitterAG/Controller/HomeDatasourceController.swift
+++ TwitterAG/Controller/HomeDatasourceController.swift
@@ -15,8 +15,22 @@ class HomeDatasourceController: DatasourceController {
         self.datasource = HomeDatasource()
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0 // no extra space betwen cells
+    }
+    
     override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 150)
+        
+        if let user = self.datasource?.item(indexPath) as? User {
+            // estimate cell size based on user bio text
+            let approximateWidthOfBioText = view.frame.width - 50 - 12 - 12 - 2
+            let size = CGSize(width: approximateWidthOfBioText, height: 1000)
+            let attributes = [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 15)]
+            
+            let estimatedFrame = NSString(string: user.bioText).boundingRect(with: size, options: .usesLineFragmentOrigin, attributes: attributes, context: nil)
+            return CGSize(width: view.frame.width, height: estimatedFrame.height + 66)
+        }
+        return CGSize(width: view.frame.width, height: 200)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
